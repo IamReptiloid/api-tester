9b48f281e025cbef091fa9cf3338ab9d60fc5825
Vladislav Ilin <ayhalrayz@gmail.com>
2021-10-15 13:17:36 +0300

    navigation

---
+++ src/components/CustomNav.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="nav">
+        <div class="nav__content">
+            <button @click="$router.push('/')" class="nav__btn">Home</button>
+            <button @click="$router.push('/Workspaces')" class="nav__btn">Workspaces</button>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .nav {
+        width: 100%;
+        color: rgb(131, 131, 131);
+        font-weight: 600;
+        font-size: 14px;
+        &::after {
+            content: '';
+            border-bottom: 1px solid rgb(230,230,230);
+            height: 1px;
+            display: block;
+            width: 100%;
+        };
+        &__content {
+            display: flex;
+        };
+        &__btn {
+            padding: 12px;
+            background-color: rgb(255,255,255);
+        };
+    }
+</style>
---
+++ src/components/UI/CustomInput.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <input type="text">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'custom-input',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
