baa44ca6f765c180ea3a295daa0dd6f0da7032bf
Vladislav Ilin <ayhalrayz@gmail.com>
2021-11-01 17:02:30 +0300

    fix

--- src/page/Workspaces.vue
+++ src/page/Workspaces.vue
@@ -37,28 +37,55 @@
 
 <script>
     import {defineAsyncComponent} from 'vue';
-    import {request} from '@/services/request'; 
+    import {ref, onMounted, reactive, computed} from 'vue'
+    import axios from 'axios';
+
 
     export default {
         setup() {
-            const {
-                requestType, 
-                requestOption, 
-                requestURL,
-                data,
-                URLWithData, 
-                changeOption, 
-                send
-            } = request()
+            const requestType = ref('');
+            const requestOption = [ 'Get', 'Post'];
+            const requestURL = ref('');
+            const data = reactive([{key: '', value: '', disable: false}]);
+
+            const URLWithData = computed(() => {
+                let temp = requestURL.value;
+
+                if(!data.length == 0) {
+                    temp = temp + '?';
+                    data.forEach(element => {
+                        if(element.disable){
+                            temp  = temp + element.key + '=' + element.value + '&';
+                        };
+                });
+                    temp = temp.slice(0, temp.length - 1);
+                }
+                
+                return temp;
+            });
+
+            const changeOption = (event) => {
+                requestType.value = event.target.value.toLowerCase()
+            };
+
+            const send = async() => {
+                try {
+                    console.log(await axios[requestType.value](URLWithData.value))
+                } catch(error) {
+                    console.log('error')
+                }
+            }
+
+            onMounted(() => {requestType.value = requestOption[0].toLowerCase()})
 
             return {
-                requestType, 
-                requestOption, 
+                requestType,
+                requestOption,
                 requestURL,
                 data,
-                URLWithData, 
-                changeOption, 
-                send
+                URLWithData,
+                changeOption,
+                send,
             }
         },
 
