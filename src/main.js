6cfe3f75d11706ab89fe44af6b2f2dfb5ceb17a0
Vladislav Ilin <ayhalrayz@gmail.com>
2021-11-01 14:12:15 +0300

    fix: deleted ui components

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import UiComponents from '@/shared/UIComponents';
 
 const app = createApp(App);
-UiComponents.forEach(UiComponents => {
-    app.component(UiComponents.name, UiComponents);
-});
+
 
 app
     .use(store)
